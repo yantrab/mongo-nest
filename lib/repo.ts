cb94acf6ba5b2d8e4b41be96f005da521ec896ad
Yaniv Trabelsi <yaniv@praedicta.com>
2019-03-19 15:33:17 +0200

    findOne & findMany

--- lib/repo.ts
+++ lib/repo.ts
@@ -4,11 +4,19 @@ import { EntityWithoutGetters } from "./EntityWithoutGetters";
 
 export class Repository<T extends Entity> {
     constructor(public collection: Collection<Partial<EntityWithoutGetters<T>>>) { }
-    saveOrUpdate(entity: Partial<EntityWithoutGetters<T>>) {
+    saveOrUpdateOne(entity: Partial<EntityWithoutGetters<T>>) {
         this.collection.updateOne({ _id: (entity as T)._id }, { $set: entity }, { upsert: true });
     }
 
     saveOrUpdateMany(entities: Partial<EntityWithoutGetters<T>>[]) {
-        this.collection.updateMany({ }, { $set: entities }, { upsert: true });
+        this.collection.updateMany({}, { $set: entities }, { upsert: true });
+    }
+
+    findMany(query?: Partial<T>): Promise<T[]> {
+        return this.collection.find<T>(query || {}).toArray();
+    }
+    
+    findOne(query?: Partial<T>): Promise<T> {
+        return this.collection.findOne<T>(query || {});
     }
 }
