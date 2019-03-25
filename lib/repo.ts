d3f04197e0665b23be6ed5d2f714ca14794f45a9
Yaniv Trabelsi <yaniv@praedicta.com>
2019-03-25 16:22:03 +0200

    save or update many fix bug

--- lib/repo.ts
+++ lib/repo.ts
@@ -5,17 +5,25 @@ import { EntityWithoutGetters } from "./EntityWithoutGetters";
 export class Repository<T extends Entity> {
     constructor(public collection: Collection<Partial<EntityWithoutGetters<T>>>) { }
     saveOrUpdateOne(entity: Partial<EntityWithoutGetters<T>>) {
-        this.collection.updateOne({ _id: (entity as T)._id }, { $set: entity }, { upsert: true });
+        return this.collection.updateOne({ _id: (entity as T)._id }, { $set: entity }, { upsert: true });
     }
 
     saveOrUpdateMany(entities: Partial<EntityWithoutGetters<T>>[]) {
-        this.collection.updateMany({}, { $set: entities }, { upsert: true });
+        if (entities.length < 1000) {
+            const bulkOperation = this.collection.initializeUnorderedBulkOp();
+            for (const item of entities) {
+                bulkOperation.find({ _id: (item as T)._id }).upsert().update(item);
+            }
+            return bulkOperation.execute();
+        }
+
+        return Promise.all(entities.map(this.saveOrUpdateOne));
     }
 
     findMany(query?: Partial<T>): Promise<T[]> {
         return this.collection.find<T>(query || {}).toArray();
     }
-    
+
     findOne(query?: Partial<T>): Promise<T> {
         return this.collection.findOne<T>(query || {});
     }
